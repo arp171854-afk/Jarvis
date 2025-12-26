5470df2e9fe3a2b20483a63b47d15bceff1a6203
arp171854-afk <arp171854@gmail.com>
2025-12-26 15:19:21 +0530

    Add function to play music on YouTube Music

---
+++ skills/music.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+def play_music():
+    """
+    Opens YouTube Music in the default browser.
+    """
+    webbrowser.open("https://music.youtube.com")
+    return "Playing music on YouTube Music."
